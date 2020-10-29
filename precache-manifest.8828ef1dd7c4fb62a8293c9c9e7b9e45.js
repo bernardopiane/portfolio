d2a7cd74de1601f2302f1f767d394d52154acc6b
bernardopiane <bernardopiane@users.noreply.github.com>
2020-10-29 11:45:41 -0300

    Updates

---
+++ precache-manifest.8828ef1dd7c4fb62a8293c9c9e7b9e45.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ddd2e898d8db651ee5af28abcf0f0a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cb227f1f3b86536675ce",
+    "url": "/portfolio/static/css/2.0cec49e1.chunk.css"
+  },
+  {
+    "revision": "bd80cfda7255d9ee3126",
+    "url": "/portfolio/static/css/main.6a810924.chunk.css"
+  },
+  {
+    "revision": "cb227f1f3b86536675ce",
+    "url": "/portfolio/static/js/2.b1a5af8a.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/portfolio/static/js/2.b1a5af8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd80cfda7255d9ee3126",
+    "url": "/portfolio/static/js/main.1db05c5a.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "ef2b3d271722c67e04ad7357a2bf5104",
+    "url": "/portfolio/static/media/appo.ef2b3d27.png"
+  },
+  {
+    "revision": "c533c00362408f4776145f1cec39976f",
+    "url": "/portfolio/static/media/bikeshop.c533c003.jpg"
+  },
+  {
+    "revision": "97596c75c87f04ec99d24bccb5b87a01",
+    "url": "/portfolio/static/media/boadaserra.97596c75.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "bcf0a593d60b4dab7b29481926e909b7",
+    "url": "/portfolio/static/media/clemo.bcf0a593.png"
+  },
+  {
+    "revision": "c8daab63e498bf6d2fc68d959f6892be",
+    "url": "/portfolio/static/media/cuda.c8daab63.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "aeea0593a014974bd647efa7933dbd53",
+    "url": "/portfolio/static/media/fullscreen.aeea0593.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "cbf6aab9855a487b94621f37ed24a080",
+    "url": "/portfolio/static/media/mint.cbf6aab9.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "063c6d91be17ce1f86a1cc1f305ae16a",
+    "url": "/portfolio/static/media/pic3.063c6d91.jpg"
+  },
+  {
+    "revision": "29fa38c9888391d0d40100479d2c4e52",
+    "url": "/portfolio/static/media/piroll.29fa38c9.jpg"
+  },
+  {
+    "revision": "f0ebd68ad60b3882559bbbcbe5d24f2f",
+    "url": "/portfolio/static/media/private-vacation.f0ebd68a.jpg"
+  },
+  {
+    "revision": "0cc635402f674e4bf0e7480b8fc5c465",
+    "url": "/portfolio/static/media/reen.0cc63540.jpg"
+  },
+  {
+    "revision": "45d020e81b3500934ea393fc25ebd14e",
+    "url": "/portfolio/static/media/tork.45d020e8.jpg"
+  },
+  {
+    "revision": "76c446b53d233c46e07eef2638cf2e18",
+    "url": "/portfolio/static/media/travel.76c446b5.png"
+  },
+  {
+    "revision": "602a2524d82bd066c37b228d93ee6332",
+    "url": "/portfolio/static/media/travelly.602a2524.jpg"
+  },
+  {
+    "revision": "a683328ded638c67f2d3b881b6ecc69e",
+    "url": "/portfolio/static/media/twobytes.a683328d.png"
+  },
+  {
+    "revision": "41beb75daeb33f46a6509bd4cee7e2f6",
+    "url": "/portfolio/static/media/weather-app-landing-page.41beb75d.png"
+  },
+  {
+    "revision": "6442181b2dee8bc73e8a9f64a6f06f1a",
+    "url": "/portfolio/static/media/z no link ss2.6442181b.png"
+  }
+]);
